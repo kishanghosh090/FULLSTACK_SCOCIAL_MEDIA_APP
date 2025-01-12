1749905f28553e710bc1461654e4d42792b3ec5a
kishanghosh090 <kishanghosh090@gmail.com>
2025-01-12 21:51:44 +0530

    like and comment on post route add

--- backend/src/routes/post.routes.js
+++ backend/src/routes/post.routes.js
@@ -5,6 +5,14 @@ import {
   updatePost,
   getAllPost,
 } from "../controllers/post.controller.js";
+import { likeOrUnlikePost } from "../controllers/like.controller.js";
+import {
+  createComment,
+  deleteComment,
+  updateComment,
+} from "../controllers/comment.controller.js";
+
+// auth middleware
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
@@ -17,5 +25,13 @@ router.delete("/deletePost", verifyJWT, deletePost);
 router.put("/updatePost", verifyJWT, updatePost);
 // get post route
 router.get("/getAllPost", getAllPost);
-
+// like post route
+router.put("/likeOrUnlikePost", verifyJWT, likeOrUnlikePost);
+// comment post route
+router.post("/commentPost", verifyJWT, createComment);
+// update comment route
+router.put("/updateComment", verifyJWT, updateComment);
+// delete comment route
+router.delete("/deleteComment", verifyJWT, deleteComment);
+// export router
 export default router;
