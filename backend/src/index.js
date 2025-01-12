154a3a25944a7ab35985bc68198246d7654a3b35
kishanghosh090 <kishanghosh090@gmail.com>
2025-01-12 20:41:07 +0530

    db connection added

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,16 @@
 import "dotenv/config";
-import { app } from "./app";
+import { app } from "./app.js";
+import connectDB from "./db/index.js";
 
 const port = process.env.PORT || 3000;
+
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
