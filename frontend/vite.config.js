1c1deddea553558d02acfcee04e3536d5aac8473
kishanghosh090 <kishanghosh090@gmail.com>
2025-01-14 22:25:38 +0530

    home page add and some bug fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,10 +4,17 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    
     proxy: {
-      "/api/v1": "http://localhost:8080",
+      "/api/v1": {
+        target: "https://fullstack-scocial-media-app.onrender.com",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
     },
+  },
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
   },
   plugins: [react()],
 });
